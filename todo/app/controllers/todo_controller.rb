2248cf32443e4bfdb263c658976811978dc8cc16
h4ppyy <93immm@naver.com>
2021-01-07 17:56:14 +0900

    complete create, delete logic

--- todo/app/controllers/todo_controller.rb
+++ todo/app/controllers/todo_controller.rb
@@ -2,16 +2,33 @@ require "content"
 
 class TodoController < ApplicationController
   def index
-    contents = Content.all
+    contents = Content.all.order(created_at: :desc)
+
     @contents = contents
   end
 
   def create
-    render :json => { name: "create" }
+    input_content = params[:input_content]
+
+    # validate
+    if input_content == ''
+      render :json => { code: 400, msg: '内容を入れてください' }
+      return false  
+    end
+
+    content = Content.new
+    content.content = input_content
+    content.save()
+
+    render :json => { code: 200 }
   end
 
   def delete
-    render :json => { name: "delete" }
+    todo_id = params[:todo_id]
+
+    Content.destroy_by(id: todo_id)
+
+    render :json => { code: 200 }
   end
 
   def playground
