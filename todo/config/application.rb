a76e240a99901cb155bdc32fdb90da4731e431bb
h4ppyy <93immm@naver.com>
2021-01-07 20:06:55 +0900

    utc to jst

--- todo/config/application.rb
+++ todo/config/application.rb
@@ -11,6 +11,9 @@ module Todo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
